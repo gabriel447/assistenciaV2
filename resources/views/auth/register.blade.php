71427c3cb35bfba7dbc5a7db1e7cb0c7c6aa5a2f
gabriel447 <gabriel447@example.com>
2026-01-29 14:41:55 -0300

    pagina principal

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,14 +39,20 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+        <div class="flex items-center justify-between mt-4">
+            <a class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150" href="{{ url('/') }}">
+                {{ __('Voltar') }}
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <div class="flex items-center">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                    {{ __('Já possui uma conta?') }}
+                </a>
+
+                <x-primary-button class="ms-4">
+                    {{ __('Cadastrar') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
 </x-guest-layout>
