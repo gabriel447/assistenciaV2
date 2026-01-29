895a10124dfcf1abe6f12df7e62623bb9e9b2eff
gabriel447 <gabriel447@example.com>
2026-01-29 15:09:07 -0300

    painel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-bold text-2xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Painel de Controle') }}
         </h2>
     </x-slot>
 
-    <div class="py-12 bg-gray-50 min-h-screen">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-8">
             
             <!-- Hero Section -->
